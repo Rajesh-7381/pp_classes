6c526c34e9ffdc810bc2208dbd5eab6eb657eea6
rajesh <aja@gmail.com>
2023-06-28 23:10:32 +0530

    msg

---
+++ validate.js
@@ -0,0 +1,10 @@
+function validate_form(){
+    var name=document.getElementById("name").value;
+    var fathername=document.getElementById("fathername").value;
+    var email=document.getElementById("email").value;
+    var phone=document.getElementById("phone").value;
+    var standard=document.getElementById("standard").value;
+    if(name < 0){
+        alert("it should be above 3");
+    }
+}
